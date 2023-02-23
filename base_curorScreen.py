16f85fdf82c23b5bad2337c5260561f57381bbb6
yotamaruyama <maruyama.youta2002@gmail.com>
2023-02-24 00:28:01 +0900

    カーソルのメソッド
    　共通動作をまとめました

---
+++ base_curorScreen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import sys
+
+import pygame
+from pygame.locals import *
+
+import cursor
+from key_parent import key_parent
+
+
+import abc
+
+class Base_CursorScreen(metaclass=abc.ABCMeta):
+
+    def __init__(self, screen,columNumber):
+
+        self.screen = screen
+        self.columNumber = columNumber
+        self.title_font = pygame.font.Font("data/minamoji04.ttf", 48)
+        self.menu_font = pygame.font.Font("data/minamoji04.ttf", 32)
+
+
+    def inputUp(self):
+        if self.select == 0:
+            return
+        else:
+            self.mycursor.move(self.screen, move_y=-self.blank)
+            self.select -= 1
+
+    def inputDown(self):
+        if self.select == self.columNumber - 1:  # カーソルが一番下まで行ったら
+            return
+        self.mycursor.move(self.screen, move_y=+self.blank)
+        self.select += 1
+    
